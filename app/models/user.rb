15338801760082ef627469d03ef0d27246158e60
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-15 12:18:19 +0400

    Maintaining Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :admin, :city, :country, :date_of_birth, :education, :email, :login, :member_of_volunteer_organizations, :name, :other_contacts, :password_digest, :phone_number, :remember_token, :surname
+  attr_accessible :city, :country, :date_of_birth, :education, :email, :login, :member_of_volunteer_organizations, :name, :other_contacts, :phone_number, :surname, :password, :password_confirmation
+  has_secure_password
+  
+  validates :login, presence: true, uniqueness: true
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, 
+	uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true	
 end
