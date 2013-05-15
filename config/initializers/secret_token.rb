451075bc32ddf4dbb697770a2de94eae70c95c1c
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-15 11:38:48 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MutualAidSocialNetwork::Application.config.secret_token = '1db83bc354d149cab743760a5f06ab55e32963bdd2aea7ce11c8fbad1efab63c00bae807af354f68f093e42a18d37f7575dec1d19c8497dbac9ed53f95760884'
