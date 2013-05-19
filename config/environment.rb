e067d96fede3e3891bc9305c1cd7c00028509740
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-19 23:55:39 +0400

    Translate Ended

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MutualAidSocialNetwork::Application.initialize!
 
-Time::DATE_FORMATS[:ru_datetime] = "%d.%m.%Y"
+#Time::DATE_FORMATS[:ru_datetime] = "%d.%m.%Y"
