43b5bebe48a9f9e7b43246e08536bce5d349191d
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-21 04:21:10 +0400

    Maintaning 'Carrierwave' for Heroku

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -51,5 +51,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
 end
